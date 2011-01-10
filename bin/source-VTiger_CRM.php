c886bc954de6d0e964b247ebb8ce7961cea5c34a
lgaetz <lgaetz@c1f4cd9e-dc08-436e-a85a-5542e7315f74>
2011-01-10 19:10:05 +0000

    field toggles added back in

--- bin/source-VTiger_CRM.php
+++ bin/source-VTiger_CRM.php
@@ -46,46 +46,70 @@ if($usage_mode == 'get caller id')
 	{
 		print "Searching vTiger ... ";
 	}
-	$numbererror = false;
-        $vtfound = false;
-	$wquery_input = "";
+
+	$wquery = "";
+        $wquery_input = "";
 	$wquery_string = "";
 	$wquery_result = "";
 
 	if (strlen($thenumber) < $run_param['Filter_Length'])
+	// Abandon search in not enough digits
 	{
 		If ($debug)
 		{
 			Print "Not enough digits";
 		}
-		$numbererror = true;
 	}
 	else
 	{
+        	//  trim incoming number to specified filter length
 		$thenumber = substr($thenumber, (-1*$run_param['Filter_Length']));
-		//  Build regular expression from $thenumber to avoid non-digit characters
-		$wquery_input = "'[^0-9]*";
+
+		//  Build regular expression from the modified $thenumber to avoid non-digit characters
+		$wquery = "'[^0-9]*";
 		for( $x=0; $x < ((strlen($thenumber))-1); $x++ )
 	   	{
-			$wquery_input .=  substr($thenumber,$x,1)."[^0-9]*" ;
+			$wquery .=  substr($thenumber,$x,1)."[^0-9]*" ;
 		}
-		$wquery_input = $wquery_input.(substr($thenumber,-1))."([^0-9]+|$)'";
-	}
+		$wquery = $wquery.(substr($thenumber,-1))."([^0-9]+|$)'";
 
-	// Connect to vTiger db
-        if (!$numbererror)
-        {
+		//  Build section of query with user specified phone fields
+		if ($run_param['Search_Office_Phone'] == "on")
+                {
+                	$wquery_input = $wquery_input." (phone REGEXP ".$wquery.") OR ";
+                }
+		if ($run_param['Search_Mobile_Phone'] == "on")
+                {
+                	$wquery_input = $wquery_input." (mobile REGEXP ".$wquery.") OR ";
+                }
+		if ($run_param['Search_Fax_Phone'] == "on")
+                {
+                	$wquery_input = $wquery_input." (fax REGEXP ".$wquery.") OR ";
+                }
+		if ($run_param['Search_Home_Phone'] == "on")
+                {
+                	$wquery_input = $wquery_input." (homephone REGEXP ".$wquery.") OR ";
+                }
+		if ($run_param['Search_Other_Phone'] == "on")
+                {
+                	$wquery_input = $wquery_input." (otherphone REGEXP ".$wquery.") OR ";
+                }
+
+		//  trim final "OR" from $wquery_input
+		$wquery_input = substr($wquery_input, 0, -3);
+
         	//  Connect to database
 		$wdb_handle = mysql_connect($run_param['DB_Host'], $run_param['DB_User'], $run_param['DB_Password']) or die("vTiger connection failed" . mysql_error());
 		mysql_select_db($run_param['DB_Name']) or die("vTiger db open error: " . mysql_error());
 		mysql_query("SET NAMES 'utf8'") or die("UTF8 set query  failed: " . mysql_error());
+
 		// search database
-       		$wquery_string = 'SELECT firstname, lastname FROM vtiger_contactdetails INNER JOIN vtiger_contactsubdetails ON vtiger_contactsubdetails.contactsubscriptionid = vtiger_contactdetails.contactid INNER JOIN vtiger_crmentity ON vtiger_crmentity.crmid = vtiger_contactdetails.contactid WHERE (phone REGEXP '.$wquery_input.') OR (mobile REGEXP '.$wquery_input.') OR (fax REGEXP '.$wquery_input.') OR (homephone REGEXP '.$wquery_input.') OR (otherphone REGEXP '.$wquery_input.') ORDER BY modifiedtime DESC';
+       		$wquery_string = 'SELECT firstname, lastname FROM vtiger_contactdetails INNER JOIN vtiger_contactsubdetails ON vtiger_contactsubdetails.contactsubscriptionid = vtiger_contactdetails.contactid INNER JOIN vtiger_crmentity ON vtiger_crmentity.crmid = vtiger_contactdetails.contactid WHERE '.$wquery_input.' ORDER BY modifiedtime DESC';
                 $wquery_result = mysql_query($wquery_string) or die("SugarCRM accounts query failed" . mysql_error());
+
+                // Close dbase connection
+               	mysql_close($wdb_handle);
 	}
-
-
-	mysql_close($wdb_handle);
 
 	if(mysql_num_rows($wquery_result)>0)
 	{
