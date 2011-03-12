bd33d70131b16f8583e3e16b6709e8aece57f9b4
lgaetz <lgaetz@c1f4cd9e-dc08-436e-a85a-5542e7315f74>
2011-03-12 17:39:22 +0000

    Added missing mouseover help

--- bin/source-MySQL_DB.php
+++ bin/source-MySQL_DB.php
@@ -19,7 +19,7 @@ $source_param['DB_Password']['type'] = 'password';
 // $source_param['SQL_Query']['desc'] = 'SQL Query used to retrieve the CNAM from the DB. Select result as data from table where telephone field like [NUMBER]. "as data" is important, NUMBER will be replaced by the called number';
 $source_param['SQL_Query']['desc'] = 'Structrue a valid MySQL query that returns the value of a single field or multiple fields concatenated into a single value with an alias of "data" (all lower case no quotes) using Select...AS.<br>Example1: SELECT name_field AS data WHERE...<br>Example2: SELECT CONCAT (firstname," ",lastname) AS data WHERE ...<br><br>Wherever you need the incoming CID number to appear in your MySQL query, substitute the string "[NUMBER]" (including square brackets, all uppercase).<br>Example: ...WHERE phone_field LIKE "[NUMBER]"...<br><br>To structure a query with a regular expression that will ignore all non-digit characters stored in the phone number field use REGEXP "[CID_REGEXP]" (including square brackets, all uppercase).<br>Example: SELECT name_field WHERE phone_field REGEXP "[CID_REGEXP]"';
 $source_param['SQL_Query']['type'] = 'text';
-$source_param['Post_Process_SQL_Query']['desc'] = 'description';
+$source_param['Post_Process_SQL_Query']['desc'] = 'Substitute "[CNAM]" (including sqare brackets all uppercase) to use the Caller Name string.  Substitute "[SOURCE]" to use the winning source.  Substitute "[NUMBER"] for the CID';
 $source_param['Post_Process_SQL_Query']['type'] = 'text';
 
 //run this if the script is running in the "get caller id" usage mode.
@@ -32,6 +32,7 @@ if($usage_mode == 'get caller id')
 	}
 	
 	$wquery_input = preg_replace("/\D/","",$thenumber); // strip non-digits
+	if (strlen($wquery_input) == 0) exit; // abandon search if no number is passed
 
 	//  Build regular expression from $thenumber to avoid non-digit characters
 	$wquery = "[^0-9]*";
@@ -41,12 +42,13 @@ if($usage_mode == 'get caller id')
 	}
 	$wquery = $wquery.(substr($thenumber,-1))."([^0-9]+|$)";
 
-	if (strlen($wquery_input) == 0) exit; // abandon search if no number is passed
 	$wdb_handle = mysql_connect($run_param['DB_Host'], $run_param['DB_User'], $run_param['DB_Password']) or die("MySQL connection failed" . mysql_error());
 	mysql_select_db($run_param['DB_Name']) or die("MySQL db open error: " . mysql_error());
 	mysql_query("SET NAMES 'utf8'") or die("UTF8 set query  failed: " . mysql_error());
 
 	$wquery_string =$run_param['SQL_Query'];
+	if (strlen($wquery_string) == 0) exit; // abandon search if no sql query is passed
+
 	$wquery_string = str_replace('[NUMBER]', $wquery_input, $wquery_string);
 	$wquery_string = str_replace('[CID_REGEXP]', $wquery, $wquery_string);
 
@@ -75,10 +77,12 @@ if($usage_mode == 'post processing')
 	mysql_select_db($run_param['DB_Name']) or die("MySQL db open error: " . mysql_error());
 	mysql_query("SET NAMES 'utf8'") or die("UTF8 set query  failed: " . mysql_error());
 
-//	[SPAM], [CNAM] perhaps [SOURCE] for
+//	swap out [SPAM], [CNAM], [SOURCE], [NUMBER]
 	$wquery_string =$run_param['Post_Process_SQL_Query'];
+	if (strlen($wquery_string) == 0) exit; // abandon search if no sql query is passed
 	$wquery_string = str_replace('[CNAM]', $first_caller_id, $wquery_string);
 	$wquery_string = str_replace('[SOURCE]', $winning_source, $wquery_string);
+        $wquery_string = str_replace('[NUMBER]', $wquery_input, $wquery_string);
 	$wquery_result = mysql_query($wquery_string) or die("MySQL query failed" . mysql_error());
 }
 
