10272ed4e886b066f203faf2709d889eede0e54f
lgaetz <lgaetz@c1f4cd9e-dc08-436e-a85a-5542e7315f74>
2011-03-12 17:04:26 +0000

    first draft to allow post processing MySQL query

--- bin/source-MySQL_DB.php
+++ bin/source-MySQL_DB.php
@@ -19,7 +19,8 @@ $source_param['DB_Password']['type'] = 'password';
 // $source_param['SQL_Query']['desc'] = 'SQL Query used to retrieve the CNAM from the DB. Select result as data from table where telephone field like [NUMBER]. "as data" is important, NUMBER will be replaced by the called number';
 $source_param['SQL_Query']['desc'] = 'Structrue a valid MySQL query that returns the value of a single field or multiple fields concatenated into a single value with an alias of "data" (all lower case no quotes) using Select...AS.<br>Example1: SELECT name_field AS data WHERE...<br>Example2: SELECT CONCAT (firstname," ",lastname) AS data WHERE ...<br><br>Wherever you need the incoming CID number to appear in your MySQL query, substitute the string "[NUMBER]" (including square brackets, all uppercase).<br>Example: ...WHERE phone_field LIKE "[NUMBER]"...<br><br>To structure a query with a regular expression that will ignore all non-digit characters stored in the phone number field use REGEXP "[CID_REGEXP]" (including square brackets, all uppercase).<br>Example: SELECT name_field WHERE phone_field REGEXP "[CID_REGEXP]"';
 $source_param['SQL_Query']['type'] = 'text';
-
+$source_param['Post_Process_SQL_Query']['desc'] = 'description';
+$source_param['Post_Process_SQL_Query']['type'] = 'text';
 
 //run this if the script is running in the "get caller id" usage mode.
 if($usage_mode == 'get caller id')
@@ -68,4 +69,17 @@ if($usage_mode == 'get caller id')
 		print "not found<br>\n";
 	}
 }
+
+if($usage_mode == 'post processing')
+{
+	mysql_select_db($run_param['DB_Name']) or die("MySQL db open error: " . mysql_error());
+	mysql_query("SET NAMES 'utf8'") or die("UTF8 set query  failed: " . mysql_error());
+
+//	[SPAM], [CNAM] perhaps [SOURCE] for
+	$wquery_string =$run_param['Post_Process_SQL_Query'];
+	$wquery_string = str_replace('[CNAM]', $first_caller_id, $wquery_string);
+	$wquery_string = str_replace('[SOURCE]', $winning_source, $wquery_string);
+	$wquery_result = mysql_query($wquery_string) or die("MySQL query failed" . mysql_error());
+}
+
 ?>
