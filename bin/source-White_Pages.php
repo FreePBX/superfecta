8b6b3fb51b88ff45952add5ee789ec9041e8510d
ukstevef <ukstevef@c1f4cd9e-dc08-436e-a85a-5542e7315f74>
2011-06-08 17:58:55 +0000

    Fix for #533 whitepages.com not returning results

--- bin/source-White_Pages.php
+++ bin/source-White_Pages.php
@@ -145,61 +145,16 @@ if($usage_mode == 'get caller id')
 		$url="http://www.whitepages.com/search/ReversePhone?full_phone=$thenumber";
 		$value = get_url_contents($url);
 		
-		$notfound = strpos($value, "PHONE_USER_ERROR");
-		$notfound = ($notfound < 1) ? strpos($value, "PHONE_NO_RESULTS") : $notfound;
-		$patternFirst = "/FIRST.*?\"(.*?)\",/";
-		$patternLast = "/LAST.*?\"(.*?)\",/";
-		$patternCity = "/CITY.*?\"(.*?)\",/";
-		$patternState = "/STATE.*?\"(.*?)\",/";
-		$patternType = "/Type: *(.*?)<\/span>/";
-		$patternCompany = "/Company: <\/strong><span class=\"org\">(.*?)<\/span>/";
-		$patternName = "/<span class=\"name\">(.*?)<\/span>/";
+		$pattern = '/<div class=\"bs_result(?: mapped_result \" id=\"result_1|)?">\s*<table class=\"result_container.*\"><tr>\s*<td class=\"result_content_container\">\s*<table>\s*<tr>\s*<td class=\"tagline_container\">\s*<p class=\"result_name\">\s*<a.*\">(.*)<\/a>/';
 		
-		// Look at named results first
-		preg_match($patternName, $value, $namespans);
-		$name = (isset($namespans[1])) ? trim(strip_tags($namespans[1])) : '';
-		
-		if($name == '')
-		{
-			//look for company name
-			preg_match($patternCompany, $value, $company);
-			$name = (isset($company[1])) ? trim(strip_tags($company[1])) : '';
-			if($name == '')
-			{
-				//now look for a first and last name
-				preg_match($patternFirst, $value, $first);
-				preg_match($patternLast, $value, $last);
-				if(isset($first[1]) && isset($last[1])){
-					$name = $first[1]." ".$last[1];
-				}
-				if($name == " ")
-				{
-				//if($debug){print "-no name-";}
-					$start= strpos($value, "</strong> based in <strong>");
-					
-					if($start > 1)
-					{
-						$value = substr($value,$start);
-						$start= strpos($value, "<strong>");
-						$value = substr($value,$start+8);
-						$end= strpos($value, "</strong>");
-						$name = substr($value,0,$end);
-						$name = str_replace( chr(13), "", $name ); 
-						$name = str_replace( chr(10), "", $name ); 
-						$name = trim(str_replace( "&nbsp;", "", $name ));
-					}
-				}
-			}
-		}
-		
-		if($notfound)
-		{
-			$name="";
+		preg_match($pattern, $value, $match);
+		if(isset($match[1]) && strlen($match[1])){
+			$name = trim(strip_tags($match[1]));
 		}
 		
 		if(strlen($name) > 1)
 		{
-			$caller_id = strip_tags($name);
+			$caller_id = $name;
 		}
 		else if($debug)
 		{
