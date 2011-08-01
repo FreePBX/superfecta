d08163213ef5252d24351691eb929c189f1e869d
Andrew Nagy <andrew.nagy@the159.com>
2011-08-01 11:16:37 -0700

    Fixed issue with astman not connecting to server on FreePBX 2.8

--- includes/config.php
+++ includes/config.php
@@ -35,4 +35,15 @@ if(file_exists("/etc/freepbx.conf")) {
 	}
 	require_once($phpasman_location);
 	$astman	= new AGI_AsteriskManager();
+	
+	// attempt to connect to asterisk manager proxy
+	if(!isset($amp_conf["ASTMANAGERPROXYPORT"]) || !$res = $astman->connect("127.0.0.1:".$amp_conf["ASTMANAGERPROXYPORT"], $amp_conf["AMPMGRUSER"] , $amp_conf["AMPMGRPASS"], 'off'))
+	{
+		// attempt to connect directly to asterisk, if no proxy or if proxy failed
+		if (!$res = $astman->connect("127.0.0.1:".$amp_conf["ASTMANAGERPORT"], $amp_conf["AMPMGRUSER"] , $amp_conf["AMPMGRPASS"], 'off'))
+		{
+			// couldn't connect at all
+			die('Could Not Connect to Asterisk Manager!');
+		}
+	}
 }
