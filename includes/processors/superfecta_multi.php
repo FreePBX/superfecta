31d8dd9fb00e9a9fc8f35debe884e16530b29dc2
ukstevef <steve4star.uk@gmail.com>
2011-08-07 01:42:14 +0100

    Move $cli, $debug and $spam to protected member variables.
    Create accessor functions for the protected variables ---  let the abstraction fun begin.

--- includes/processors/superfecta_multi.php
+++ includes/processors/superfecta_multi.php
@@ -4,7 +4,7 @@ class superfecta_multi extends superfecta_base {
 	function __construct($multifecta_id,$db,$amp_conf,$astman,$debug,$thenumber_orig,$scheme_name,$scheme_param,$source) {
 		//Check if we are a multifecta child, if so, get our variables from our child record
 		$this->multifecta_id = $multifecta_id;
-		$this->debug = $debug;
+		$this->setDebug($debug);
 		$sn = explode("_", $scheme_name);
 		$this->scheme_name = $sn[1];
 		$this->scheme = $scheme_name;
@@ -37,7 +37,7 @@ class superfecta_multi extends superfecta_base {
 				$this->DID = $row['extension'];
 				$this->multifecta_parent_id = $row['superfecta_mf_id'];
 				if($row['debug']){
-					$this->debug = true;
+					$this->setDebug(true);
 				}
 				$this->single_source = $row['source'];
 			}else{
@@ -81,7 +81,7 @@ class superfecta_multi extends superfecta_base {
 				".$this->db->quoteSmart($this->thenumber_orig).",
 				".$this->db->quoteSmart('NULL').",
 				".$this->db->quoteSmart('PREFIX').",
-				".$this->db->quoteSmart(($this->debug)?'1':'0')."
+				".$this->db->quoteSmart(($this->isDebug())?'1':'0')."
 			)";
 		// Create the parent record
 		$res2 = $this->db->query($query);
@@ -94,11 +94,9 @@ class superfecta_multi extends superfecta_base {
 		if($superfecta_mf_id = (($this->amp_conf["AMPDBENGINE"] == "sqlite3") ? sqlite_last_insert_rowid($this->db->connection) : mysql_insert_id($this->db->connection)))
 		{
 			// We have the parent record id
-			if($this->debug) {
-				$this->outn("Multifecta Parent ID:".$superfecta_mf_id."<br>\n");
-			}
+			$this->DebugPrint("Multifecta Parent ID:".$superfecta_mf_id);
 		}else{
-			die("Unable to get parent record id<br>");
+			$this->DebugDie("Unable to get parent record id");
 		}
 		$sources = explode(",",$this->scheme_param['sources']);
 		$multifecta_count = 1;
@@ -121,7 +119,7 @@ class superfecta_multi extends superfecta_base {
 				die("Unable to create child record: " . $res2->getMessage() .  "<br>");
 			}
 			if($superfecta_mf_child_id = (($this->amp_conf["AMPDBENGINE"] == "sqlite3") ? sqlite_last_insert_rowid($this->db->connection) : mysql_insert_id($this->db->connection))){
-				if($this->debug){
+				if($this->isDebug()){
 					$this->outn("Spawning child ".$superfecta_mf_child_id.":". $data);
 					exec('/usr/bin/php /var/www/html/admin/modules/superfecta/includes/callerid.php -s '.$this->scheme_name.' -n '.$this->thenumber_orig.' -m ' . $superfecta_mf_child_id . ' -r '.$data.' > log-'.$superfecta_mf_child_id.'.log 2>&1 &');
 				} else {
@@ -131,9 +129,7 @@ class superfecta_multi extends superfecta_base {
 			}
 			$multifecta_count++;
 		}
-		if($this->debug){
-			$this->outn("Parent took ".number_format(($this->mctime_float()-$multifecta_start_time),4)." seconds to spawn children.<br>\n");
-		}
+		$this->DebugPrint("Parent took ".number_format(($this->mctime_float()-$multifecta_start_time),4)." seconds to spawn children.");
 		$query = "SELECT superfecta_mf_child_id, priority, cnam, spam_text, spam, source, cached
 				FROM superfecta_mf_child
 				WHERE superfecta_mf_id = ".$this->db->quoteSmart($superfecta_mf_id)."
@@ -181,16 +177,14 @@ class superfecta_multi extends superfecta_base {
 				){
 					if((!$multifecta_timeout_hit) && (($loop_cur_time - $loop_start_time)>$loop_priority_time_limit)){
 						$multifecta_timeout_hit = true;
-						if($this->debug){
-							$this->outn("Multifecta Timeout reached.  Taking first child with a CNAM result.");
-						}
+						$this->DebugPrint("Multifecta Timeout reached.  Taking first child with a CNAM result.");
 					}
 					// Record the results of any spam sources
 					// We dont break out of the loop for spam though.  We'll just keep
 					// checking it over and over until we get a cnam or we time-out.
 					$spam_text = (($row2['spam_text'])?$row2['spam_text']:$spam_text);
 					if($row2['spam']){
-						$this->spam = TRUE;
+						$this->setSpam(TRUE);
 						$spam_text = $row2['spam_text'];
 						$spam_source = $row2['source'];
 						$spam_child_id = $row2['superfecta_mf_child_id'];
@@ -213,14 +207,12 @@ class superfecta_multi extends superfecta_base {
 			if($loop_limit && ($loop_cur_time - $loop_start_time)<=$loop_time_limit){
 				usleep(50000); // sleep for 1/20 second. Short delay, but should help from taxing the system too much.
 			}else{
-				if($this->debug){
-					$this->outn("Maximum timeout reached.  Will not wait for any more children.");
+					$this->DebugPrint("Maximum timeout reached.  Will not wait for any more children.");
 					break;
-				}
 			}
 		}			
 		
-		if($this->debug) {			
+		if($this->isDebug()) {			
 			$sql = 'SELECT superfecta_mf_child_id, source FROM superfecta_mf_child WHERE superfecta_mf_id = '.$superfecta_mf_id;
 			$list =& $this->db->getAll($sql, array(), DB_FETCHMODE_ASSOC);
 			usleep(50000);
@@ -249,17 +241,17 @@ class superfecta_multi extends superfecta_base {
 				";
 		$res2 = $this->db->query($query);
 					
-		if($this->debug && $loop_cur_time){
-			$this->outn("Parent waited " . number_format(($loop_cur_time - $loop_start_time),4) . " seconds for children's results.");
+		if($loop_cur_time){
+			$this->DebugPrint("Parent waited " . number_format(($loop_cur_time - $loop_start_time),4) . " seconds for children's results.");
 		}
-		if($this->debug && $first_caller_id){
-			$this->outn("Winning CNAM child source ".$winning_child_id.":". $winning_source.", with: ".$first_caller_id);
+		if($first_caller_id){
+			$this->DebugPrint("Winning CNAM child source ".$winning_child_id.":". $winning_source.", with: ".$first_caller_id);
 		}
-		if($this->debug && $spam_text){
-			$this->outn("Winning SPAM child source ".$spam_child_id.":". $spam_source);
+		if($spam_text){
+			$this->DebugPrint("Winning SPAM child source ".$spam_child_id.":". $spam_source);
 		}
-		if($this->debug && (!$first_caller_id) && (!$spam_text)){
-			$this->outn("No winning SPAM or CNAM children found in allotted time.");
+		if((!$first_caller_id) && (!$spam_text)){
+			$this->DebugPrint("No winning SPAM or CNAM children found in allotted time.");
 			return(FALSE);
 		}	
 		return($first_caller_id);		
@@ -279,14 +271,14 @@ class superfecta_multi extends superfecta_base {
 			require_once($source_file);
 			$source_class = NEW $this->source;
 			//Gotta be a better way to do this
-			$source_class->debug = $this->debug;
+			$source_class->setDebug($this->isDebug());
 			$source_class->amp_conf = $this->amp_conf;
 			$source_class->db = $this->db;
 			$source_class->astman = $this->astman;
 			$source_class->thenumber_orig = $this->thenumber_orig;
 			if(method_exists($source_class, 'get_caller_id')) {
 				$caller_id = $source_class->get_caller_id($this->thenumber,$run_param);
-				$this->spam = $source_class->spam;
+				$this->setSpam($source_class->isSpam());
 				$this->cache_found = $source_class->cache_found;
 				unset($source_class);
 				$caller_id = $this->_utf8_decode($caller_id);
@@ -296,10 +288,7 @@ class superfecta_multi extends superfecta_base {
 				}
 				if(($this->first_caller_id == '') && ($caller_id != ''))
 				{
-					if($this->debug)
-					{
-						echo "<br/>Returned Result was: ".$caller_id;
-					}
+					$this->DebugEcho( "<br/>Returned Result was: ".$caller_id);
 				}
 				$end_time_whole = $this->mctime_float();
 				
@@ -314,7 +303,7 @@ class superfecta_multi extends superfecta_base {
 							$query .= ",
 							spam_text = ".$this->db->quoteSmart($this->spam_text);
 						}
-						if($this->spam){
+						if($this->isSpam()){
 							$query .= ",
 							spam = ".$this->db->quoteSmart($this->spam);
 						}
@@ -329,11 +318,11 @@ class superfecta_multi extends superfecta_base {
 				if (DB::IsError($res)){
 					die("Unable to update child: " . $res->getMessage() .  "<br>");
 				}
-			} elseif($this->debug) {
-				print "Function 'get_caller_id' does not exist!<br>\n";
+			} else {
+				$this->DebugPrint( "Function 'get_caller_id' does not exist!" );
 			}
-		} elseif($this->debug) {
-			print "Unable to find source '".$this->source."' skipping..<br\>\n";
+		} else {
+			$this->DebugPrint( "Unable to find source '".$this->source."' skipping..");
 		}
 	}
 	
@@ -341,10 +330,8 @@ class superfecta_multi extends superfecta_base {
 
 		$sources = explode(",",$this->scheme_param['sources']);
 		
-		if($this->debug)
-		{
-			$this->outn("Post CID retrieval processing.");
-		}	
+		$this->DebugPrint("Post CID retrieval processing.");
+
 		foreach($sources as $source_name)
 		{
 			// Run the source
@@ -355,7 +342,7 @@ class superfecta_multi extends superfecta_base {
 				require_once("source-".$source_name.".module");
 				$source_class = NEW $source_name;
 				$source_class->db = $this->db;
-				$source_class->debug = $this->debug;
+				$source_class->setDebug($this->isDebug());
 				if(method_exists($source_class, 'post_processing')) {					
 					$caller_id = $source_class->post_processing(FALSE,NULL,$caller_id,$run_param,$this->thenumber_orig);
 				} else {
