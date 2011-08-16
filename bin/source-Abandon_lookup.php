1a51fe5173d3e9a210e3b1e394840cee5bc53318
Lorne Gaetz <lgaetz@tru.eastlink.ca>
2011-08-16 10:22:27 -0300

    Added check for NULL Artificial CNAM

--- bin/source-Abandon_lookup.php
+++ bin/source-Abandon_lookup.php
@@ -5,7 +5,7 @@
 // CID Superfecata lookup to stop a search from progressing.
 
 //  Collect info
-	$source_desc = "Artificially sets the CNAM to a user defined value so that no further lookups will be performed.  This can be useful to prevent a lookup from progressing to the next scheme, or to always keep post-processing within the same scheme.  Lookup sources that come after this source, within the same scheme, will be ignored.";
+	$source_desc = "Artificially sets the CNAM to a user defined value so that no further lookups will be performed";
 	$source_param['Artificial_CNAM']['desc'] = 'Text string to be used as the CNAM';
 	$source_param['Artificial_CNAM']['type'] = 'text';
 	$source_param['Artificial_CNAM']['default'] = 'Unknown';
@@ -15,6 +15,10 @@
 //  run this if the script is running in the "get caller id" usage mode.
 if($usage_mode == 'get caller id')
 {
+	if ($run_param['Artificial_CNAM'] == NULL)
+        {
+		$run_param['Artificial_CNAM'] = $source_param['Artificial_CNAM']['default'];
+        }
 
 	if($debug)
 	{
