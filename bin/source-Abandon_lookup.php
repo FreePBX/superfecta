263ef515c13aba339143091f4cbf2a588b4dcdc1
Lorne Gaetz <lgaetz@tru.eastlink.ca>
2011-08-16 09:35:35 -0300

    Edited bin/source-Abandon_lookup.php via GitHub

--- bin/source-Abandon_lookup.php
+++ bin/source-Abandon_lookup.php
@@ -6,9 +6,9 @@
 
 //  Collect info
 	$source_desc = "Artificially sets the CNAM to a user defined value so that no further lookups will be performed.  This can be useful to prevent a lookup from progressing to the next scheme, or to always keep post-processing within the same scheme.  Lookup sources that come after this source, within the same scheme, will be ignored.";
-	$source_param['artificial']['desc'] = 'Text string to be used as the CNAM';
-	$source_param['artificial']['type'] = 'text';
-	$source_param['artificial']['default'] = 'Unknown';
+	$source_param['Artificial_CNAM']['desc'] = 'Text string to be used as the CNAM';
+	$source_param['Artificial_CNAM']['type'] = 'text';
+	$source_param['Artificial_CNAM']['default'] = 'Unknown';
 
 
 
