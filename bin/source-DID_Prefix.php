3c8a847c68cab3c4342576c4472fba4496a815b7
Lorne Gaetz <lgaetz@tru.eastlink.ca>
2011-10-28 16:23:00 -0300

    Ready for testing.  Not sure if it is a good idea to set the $prefix variable, comments?

--- bin/source-DID_Prefix.php
+++ bin/source-DID_Prefix.php
@@ -3,16 +3,16 @@
 //If a valid match is found, it will give $caller_id a value
 //available variables for use are: $thenumber, $thenumber_orig
 //Based largely on trunk provided lookup source
-//Revised, October 28, 2011
+//Created, October 28, 2011
 
 //configuration / display parameters
 //The description cannot contain "a" tags, but can contain limited HTML. Some HTML (like the a tags) will break the UI.
 $source_desc = "Locates the DID provided by the trunk, and then uses all or some of those digits as a prefix for the Caller ID Name.  This lookup source requires Superfecta Module version 2.2.1 or higher.";
 $source_param = array();
-$source_param['Filter_Length']['desc'] = 'Number of rightmost digits of DID to use.  Enter false to disable';
+$source_param['Filter_Length']['desc'] = 'Number of rightmost digits of returned DID to use for the Caller ID Name or Prefix.  Enter false to disable';
 $source_param['Filter_Length']['type'] = 'number';
 $source_param['Filter_Length']['default'] = 10;
-$source_param['Use_Prefix']['desc'] = 'Use returned DID digits as a Name prefix';
+$source_param['Use_Prefix']['desc'] = 'If enabled, the returned DID digits will be used as a prefix to the Caller ID name.';
 $source_param['Use_Prefix']['type'] = 'checkbox';
 $source_param['Use_Prefix']['default'] = "on";
 
@@ -26,8 +26,8 @@ if($usage_mode == 'get caller id')
 
 	$provided_did = '';
 
-	//get the caller id name already set for this call using the PHP Asterisk Manager
-	//first get a list of the active channels and return the first one that has a caller id value set.
+	//get the DID for this call using the PHP Asterisk Manager
+	//first get a list of the active channels and return the first one that matches the caller id value set.
 	$value = $astman->command('core show channels concise');
 	$chan_array = preg_split("/\n/",$value['data']);
 	foreach($chan_array as $val)
@@ -57,7 +57,7 @@ if($usage_mode == 'get caller id')
 		}
 	}
 
-        // if no DID is set, asterisk will return (N/A) check for this case and reset
+        // if there is no DID for the call, asterisk will return (N/A) check for this case and reset
 	if ($provided_did == '(N/A)')
         {
         	$provided_did = '';
@@ -74,8 +74,24 @@ if($usage_mode == 'get caller id')
 			print "found DID of $provided_did ... ";
 		}
 
-//  need to trim the DID to the $run_param['Filter_Length'] if specified
-//  neet to set $caller_id or CNAM prefix depending on user input $run_param['use_prefix']
+		// check if user wants has set the filter length
+        	if ($run_param['Filter_Length'] != false)
+        	{
+                	// keep only the filter_length rightmost digits
+        		if (strlen($provided_did) > $run_param['Filter_Length']) $provided_did = substr($provided_did, -$run_param['Filter_Length']);
+		}
+
+
+		//  neet to set $caller_id or CNAM prefix depending on user input
+        	if ($run_param['Use_Prefix'] == "on")
+                {
+                	//  Not sure the following line is a good idea, it works but may interfere with superfecta operation
+                	$prefix = $provided_did;
+                }
+                else
+                {
+                	$caller_id = $provided_did;
+                }
 
 	}
 }
