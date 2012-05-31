0296b2bc0e499600b602a6bccc4ca20ac5d3bdae
Lorne Gaetz <lgaetz@tru.eastlink.ca>
2012-05-31 14:21:15 -0300

    Final fix for issue #75

--- bin/source-Yellowpages_AU.php
+++ bin/source-Yellowpages_AU.php
@@ -1,8 +1,9 @@
 <?php
 //this file is designed to be used as an include that is part of a loop.
-//If a valid match is found, it should give $caller_id a value
+//If a valid match is found, it will give $caller_id a value
 //available variables for use are: $thenumber
 //retreive website contents using get_url_contents($url);
+// Last edited May 31, 2012
 
 //configuration / display parameters
 //The description cannot contain "a" tags, but can contain limited HTML. Some HTML (like the a tags) will break the UI.
@@ -15,7 +16,7 @@ if($usage_mode == 'get caller id')
 
 	if($debug)
 	{
-		print "Searching yellowpages.com.au ... ";
+		print "Searching yellowpages.com.au with google... ";
 	}
 	
 	// Validate number
@@ -32,8 +33,19 @@ if($usage_mode == 'get caller id')
 		$num2 = substr($thenumber,4,3);
 		$num3 = substr($thenumber,7,3);
 		$fullnum = $num1." ".$num2." ".$num3;
+	}elseif($match = match_pattern("1300XXXXXX",$thenumber)){
+		// 1300 XXX XXX number
+		$num1 = substr($thenumber,0,4);
+		$num2 = substr($thenumber,4,3);
+		$num3 = substr($thenumber,7,3);
+		$fullnum = $num1." ".$num2." ".$num3;
+	}elseif($match = match_pattern("13XXXX",$thenumber)){
+		// 13 XXXX number
+		$num1 = substr($thenumber,0,2);
+		$num2 = substr($thenumber,2,4);
+		$num3 = "";
+		$fullnum = $num1." ".$num2;
 	}else{
-//  this section is catching valid numbers of the form 1300 xxx xxx, and perhaps other formats as well
 		$number_error = true;
 	}
 	
@@ -51,20 +63,21 @@ if($usage_mode == 'get caller id')
 		$name = "";
 
 		// Pull 30 (more than 10) results to have a better change of harvesting name directly from google results
-		$url = "http://www.google.com/search?num=30&hl=en&lr=&safe=off&as_qdr=all&q=%22".$num1."+".$num2."+".$num3."%22+site%3Awww.yellowpages.com.au+-site%3Awww.yellowpages.com.au%2Fsearch&btnG=Search";
+//		$url = "http://www.google.com/search?num=30&hl=en&lr=&safe=off&as_qdr=all&q=%22".$num1."+".$num2."+".$num3."%22+site%3Awww.yellowpages.com.au+-site%3Awww.yellowpages.com.au%2Fsearch&btnG=Search";   //working Dec 2010
+		$url = "http://www.google.com/search?q=(".$num1.")+".$num2."+".$num3."+-similar+site%3Ayellowpages.com.au&amp;ie=utf-8&amp;oe=utf-8&amp;aq=t&amp;rls=org.mozilla:en-US:official&amp;client=firefox-a";  //working May 31, 2012
 
-		//$url = "http://www.google.com/search?q=%22".$num1."+".$num2."+".$num3."%22+site:www.yellowpages.com.au";
 		$value = get_url_contents($url);
 
-		// First, check if we can just pull the name directly from google without having to pull the slow yellopages.com.au page
+		// Check if we can just pull the name directly from google without having to pull the slow yellopages.com.au page
 //		$pattern = "/<a href=\"http:\/\/www\.yellowpages\.com\.au\/(?!find|search)[^\"]{1,100}\"[^>]{1,100}>([^-<]{1,100}).{1,100}<div class=\"s\">[^:<]{1,200}<em>".$num1.".{0,1} ".$num2." ".$num3."/i";   //working as of Dec 2010
-		$pattern ="/<a href=\"\/url\?q=http:\/\/www\.yellowpages\.com\.au\/.+\">(.{2,30})[ ]-[ ].+Phone number .<b>".$num1.".[ ]".$num2."[ ]".$num3."/";    //working as of May 30, 2012
-
+		$pattern ="/<a href=\"\/url\?q=http:\/\/www\.yellowpages\.com\.au\/[^f].+?\">(.{3,40}?) - .{1,200}$num1.{0,20}$num2.{0,10}$num3<\/b>\./";    //working as of May 31, 2012
 
 		if(preg_match($pattern, $value, $match)){
 			// Found a usable name in googles search results, use it
 			$name = trim(strip_tags($match[1]));
 		}else{
+
+/*****  Temporarily removing the deep search section since it is not working as of May 31, 2012 *******
 			// Looks like we'll have to take the slow route
 			if($debug){print "Using deep search for yellowpages.com.au ... ";}
 			// Check to see if google thinks there is a result on "www.yellowpages.com.au/find" 
@@ -101,6 +114,7 @@ if($usage_mode == 'get caller id')
 					print "Found on google, but something is wrong with the source page.<br>\n";
 				}
 			}
+*****  End of temporary removal *******/
 		}
 
 		// If we found a match, return it
