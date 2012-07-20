7eb74026bfc3fded20d23ae3c6175c4a3ae979ce
Andrew Nagy <andrew.nagy@the159.com>
2012-07-20 01:24:25 -0700

    Major Updates -Completely redid sources settings. --Removed the function settings(), Using class statics instead -Fixed CID Rules -Fixed DID --Now shows DID test box -Strip all non digit characters using preg_replace -Fix link at bottom of page to no longer reference me (@tm1000) -Allow Enter key to be pressed on both the DID text box and the CID test box which will automatically submit the debug form -Move module from 'inbound call control' to 'other' heading for FreePBX2.10 compatibility

--- sources_new.php
+++ sources_new.php
@@ -1,92 +1,61 @@
 <?php
 
 define("UPDATE_SERVER", "https://raw.github.com/tm1000/Caller-ID-Superfecta/v3.x/sources/");
-define("ROOT_PATH", dirname(__FILE__).'/');
+define("ROOT_PATH", dirname(__FILE__) . '/');
 
 //fix in the future
 $version = preg_replace('/(alpha|beta)/i', '.0.', $module_info['module']['version']);
-
-//Check for updates if enabled
-if($check_updates == 'on')
-{
-	$update_array = array();
-	// Load files available on live update
-	if(($check_updates == 'on') || ($update_file != ''))
-	{
-		$update_array = array();
-		$source_list = $superfecta->xml2array(UPDATE_SERVER.'source-list.xml');
-                
-                if(array_key_exists('source', $source_list['data'])) {
-                    foreach($source_list['data']['source'] as $sources) {
-                            if($sources['version_requirement'] <= $version) {
-                                $this_source_name = substr(substr(trim($sources['name']),7),0,-7);
-                                $update_array[$this_source_name]['link'] = UPDATE_SERVER.$sources['name'];
-                                $update_array[$this_source_name]['date'] = $sources['modified'];
-                                $update_array[$this_source_name]['md5'] = $sources['md5'];
-                                $update_array[$this_source_name]['version_requirement'] = $sources['version_requirement'];
-                            } else {
-                                $this_source_name = substr(substr(trim($sources['name']),7),0,-7);
-                                $update_array[$this_source_name]['outdated'] = $sources['version_requirement'];
-                            }
-                    }
-                } else {
-                    $update_site_unavailable = true;
-                    $check_updates = 'off';
-                }
-		
-	}	
-}
+preg_match('/^(\d\.\d)/i', $version, $matches);
+$major_version = $matches[1];
 
 //Get the enabled sources from this scheme
 $sql = "SELECT value FROM superfectaconfig WHERE source='$scheme' AND field='sources'";
-$enabled_sources = explode(',',$db->getOne($sql));
+$enabled_sources = explode(',', $db->getOne($sql));
 
 //get a list of the files that are on this local server
 $tpl_sources = array();
 $i = 0;
 foreach (glob(ROOT_PATH . "sources/source-*.module") as $filename) {
-    if ($filename != '') {
+    if (file_exists($filename)) {
         $source_desc = '';
         $source_param = array();
 
         require_once($filename);
 
         preg_match('/source-(.*)\.module/i', $filename, $matches);
-
         $this_source_name = $matches[1];
-        $source_class = NEW $this_source_name;
-        $settings = $source_class->settings();
 
-        $j = !in_array($this_source_name, $enabled_sources) ? ($j = $i + 200) : ($j = $i);
-        if(in_array($this_source_name, $enabled_sources)) {
-            $j = array_search($this_source_name, $enabled_sources);
-        } else {
-            $j = $i + 200;
-        }
-        $tpl_sources[$j]['showup'] = FALSE;
-        $tpl_sources[$j]['showdown'] = FALSE;
-        $tpl_sources[$j]['showupdate'] = FALSE;
-        $tpl_sources[$j]['filename'] = $filename;
-        $tpl_sources[$j]['pretty_source_name'] = str_replace("_"," ", $this_source_name);
-        $tpl_sources[$j]['source_name'] = $this_source_name;
-        $tpl_sources[$j]['settings'] = $settings;
-        $tpl_sources[$j]['enabled'] = in_array($this_source_name, $enabled_sources) ? TRUE : FALSE;
-        $tpl_sources[$j]['status'] = in_array($this_source_name, $enabled_sources) ? 'enabled' : 'disabled';
-        $tpl_sources[$j]['description'] = isset($settings['desc']) ? $settings['desc'] : 'N/A';
-        $tpl_sources[$j]['show_link'] = isset($settings['source_param']) ? TRUE : FALSE;
+        if ($major_version >= $this_source_name::$version_requirement) {
 
-        //Simplify please
-        if(in_array($this_source_name, $enabled_sources)) {
-            if($enabled_sources[0] != $this_source_name) {
-                $tpl_sources[$j]['showup'] = TRUE;
+            $j = !in_array($this_source_name, $enabled_sources) ? ($j = $i + 200) : ($j = $i);
+            if (in_array($this_source_name, $enabled_sources)) {
+                $j = array_search($this_source_name, $enabled_sources);
+            } else {
+                $j = $i + 200;
             }
-            $c = count($enabled_sources) - 1;
-            if($enabled_sources[$c] != $this_source_name) {
-                $tpl_sources[$j]['showdown'] = TRUE;
-            }
-        }
+            $tpl_sources[$j]['showup'] = FALSE;
+            $tpl_sources[$j]['showdown'] = FALSE;
+            $tpl_sources[$j]['showupdate'] = FALSE;
+            $tpl_sources[$j]['pretty_source_name'] = str_replace("_", " ", $this_source_name);
+            $tpl_sources[$j]['source_name'] = $this_source_name;
+            $tpl_sources[$j]['enabled'] = in_array($this_source_name, $enabled_sources) ? TRUE : FALSE;
+            $tpl_sources[$j]['status'] = in_array($this_source_name, $enabled_sources) ? 'enabled' : 'disabled';
+            $tpl_sources[$j]['description'] = isset($this_source_name::$description) ? preg_replace('/(<a>|<\/a>)/i', '', $this_source_name::$description) : 'N/A';
+            $tpl_sources[$j]['show_link'] = isset($this_source_name::$source_param) ? TRUE : FALSE;
 
-        $i++;
+            //Simplify please
+            if (in_array($this_source_name, $enabled_sources)) {
+                if ($enabled_sources[0] != $this_source_name) {
+                    $tpl_sources[$j]['showup'] = TRUE;
+                }
+                $c = count($enabled_sources) - 1;
+                if ($enabled_sources[$c] != $this_source_name) {
+                    $tpl_sources[$j]['showdown'] = TRUE;
+                }
+            }
+
+            $i++;
+        }
     }
 }
 
@@ -94,12 +63,12 @@ ksort($tpl_sources);
 
 $supertpl->assign("scheme", $scheme);
 
-$supertpl->assign( "check_updates_check", ($check_updates == 'on') ? 'checked' : '' );
+$supertpl->assign("check_updates_check", ($check_updates == 'on') ? 'checked' : '' );
 
-$supertpl->assign( "check_updates_check", ($update_site_unavailable) ? 'Update Server Unavalible' : '' );
+$supertpl->assign("check_updates_check", ($update_site_unavailable) ? 'Update Server Unavalible' : '' );
 
-$supertpl->assign( "sources" , $tpl_sources);
+$supertpl->assign("sources", $tpl_sources);
 
-$supertpl->assign( "web_path" , 'http://'.$_SERVER['SERVER_NAME'].'/admin/modules/superfecta/tpl/js/jquery.form.js');
+$supertpl->assign("web_path", 'http://' . $_SERVER['SERVER_NAME'] . '/admin/modules/superfecta/tpl/js/jquery.form.js');
 
-echo $supertpl->draw( 'sources' );                
+echo $supertpl->draw('sources');
