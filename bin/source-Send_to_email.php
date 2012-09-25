ce149fe7f8ff0a9e69eb39352eedd57f8c525a7b
Lorne Gaetz <lgaetz@tru.eastlink.ca>
2012-09-25 14:59:48 -0300

    Send to email improvements

--- bin/source-Send_to_email.php
+++ bin/source-Send_to_email.php
@@ -11,9 +11,12 @@ $source_param = array();
 $source_param['Email_address']['desc'] = 'Specify email address';
 $source_param['Email_address']['type'] = 'text';
 $source_param['Email_address']['default'] = '';
-$source_param['Message_Body']['desc'] = 'Content for the body of the email';
+$source_param['Message_Subject']['desc'] = 'Content for the subject of the email.  Substitute [NUMBER] and [NAME] for CID variables.';
+$source_param['Message_Subject']['type'] = 'text';
+$source_param['Message_Subject']['default'] = 'Incoming call: [NAME] [NUMBER]';
+$source_param['Message_Body']['desc'] = 'Content for the body of the email. Substitute [NUMBER] and [NAME] for CID variables.';
 $source_param['Message_Body']['type'] = 'text';
-$source_param['Message_Body']['default'] = '';
+$source_param['Message_Body']['default'] = 'Incoming call: [NAME] [NUMBER]';
 $source_param['Default_CNAM']['desc'] = 'Text to push if no CNAM is found. Leave blank to prevent Superfecta from sending anything if no CNAM is found.';
 $source_param['Default_CNAM']['type'] = 'text';
 $source_param['Default_CNAM']['default'] = '';
@@ -35,9 +38,15 @@ if($usage_mode == 'post processing')
 
 	if (($run_param['Email_address'] !='') && ($cnam != ''))
 	{
-		$subject = '"Incoming call: '.$cnam.' '.$thenumber.'"';
+		$subject = $run_param['Message_Subject'];
+		$subject = str_replace('[NAME]', $cnam, $subject);
+		$subject = str_replace('[NUMBER]', $thenumber_orig, $subject);
+		$body = $run_param['Message_Body'];
+		$body = str_replace('[NAME]', $cnam, $body);
+		$body = str_replace('[NUMBER]', $thenumber_orig, $body);
+//		$subject = 'Incoming call: '.$cnam.' '.$thenumber.'';
 //		shell_exec('mail -s '.$subject.' '.$run_param['Email_address'].' < /dev/null');
-		mail ($run_param['Email_address'], $subject, $run_param['Message_Body']);
+		mail ($run_param['Email_address'], $subject, $body);
 		if($debug)
 		{
 			print 'Send to '.$run_param['Email_address'].': '.$subject.'<br><br>';
