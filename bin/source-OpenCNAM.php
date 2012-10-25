7ccd0f52a7cb04945d3e40c19136cf3dc90531a0
Lorne Gaetz <lgaetz@tru.eastlink.ca>
2012-10-25 10:26:36 -0300

    2.x fix for #106.  Need confirmation that username/key works

--- bin/source-OpenCNAM.php
+++ bin/source-OpenCNAM.php
@@ -3,7 +3,7 @@
 //If a valid match is found, it will give $caller_id a value
 //available variables for use are: $thenumber
 //retreive website contents using get_url_contents($url);
-//Last edited Aug. 14, 2012 by lgaetz
+//Last edited Oct. 25, 2012 by lgaetz
 
 //configuration / display parameters
 //The description cannot contain "a" tags, but can contain limited HTML. Some HTML (like the a tags) will break the UI.
@@ -15,79 +15,63 @@ $source_param['Username']['default'] = null;
 $source_param['API']['desc'] = "Enter OpenCNAM user account API.  If you don't have an account leave this blank to use free service.";
 $source_param['API']['type'] = 'text';
 $source_param['API']['default'] = null;
+$source_param['Ignore_Keywords']['desc'] = 'If this source provides CNAM including any of the keywords listed  here, the CNAM will be ignored and other sources will be used to find the value.<br>
+Seperate keywords with commas.';
+$source_param['Ignore_Keywords']['type'] = 'textarea';
+$source_param['Ignore_Keywords']['default'] = 'unavailable';
 
 
 //run this if the script is running in the "get caller id" usage mode.
 if($usage_mode == 'get caller id')
 {
-    $number_error = false;
     if($debug)
     {
             print 'Searching OpenCNAM..<br> ';
     }
 
-    //check for the correct 11 digits in US/CAN phone numbers in international format.
-    // country code + number
-    if (strlen($thenumber) == 11)
-    {
-        if (substr($thenumber,0,1) == 1)
+		if ($run_param['Username'] == null or $run_param['API'] == null)  //use free url
         {
-            $thenumber = substr($thenumber,1);
-        }
-        else
-        {
-            $number_error = true;
-        }
-
-    }
-    // international dialing prefix + country code + number
-    if (strlen($thenumber) > 11)
-    {
-        if (substr($thenumber,0,3) == '001')
-        {
-            $thenumber = substr($thenumber, 3);
-        }
-        else
-        {
-            if (substr($thenumber,0,4) == '0111')
-            {
-                $thenumber = substr($thenumber,4);
-            }
-            else
-            {
-                $number_error = true;
-            }
-        }
-
-    }
-
-    if(strlen($thenumber) < 10)
-    {
-        $number_error = true;
-    }
-
-    $thenumber = (substr($thenumber,0,1) == 1) ? substr($thenumber,1) : $thenumber;
-
-    if(!$number_error)
-    {
-    	if ($run_param['Username'] == null or $run_param['API'] == null)  //use free url
-        {
-			$url = "https://api.opencnam.com/v1/phone/" . $thenumber . "?format=pbx";
+			$url = "https://api.opencnam.com/v2/phone/" . $thenumber . "?format=pbx";
         }
 		else  //use premium url
         {
-			$url = "https://api.opencnam.com/v1/phone/" . $thenumber . "?format=text&username=".$run_param['Username']."&api_key=".$run_param['API'];
+			$url = "https://api.opencnam.com/v2/phone/" . $thenumber . "?format=pbx&username=".$run_param['Username']."&api_key=".$run_param['API'];
         }
-
         $sname =  get_url_contents($url);
-
+		$sname = trim(strip_tags($sname));
         if (strlen($sname) > 1)
         {
-            $caller_id = trim(strip_tags($sname));
+			// convert list of ignore keywords into array
+			$key_words = array();
+			$temp_array = explode(',',(isset($run_param['Ignore_Keywords'])?$run_param['Ignore_Keywords']:$source_param['Ignore_Keywords']['default']));
+			foreach($temp_array as $val)
+			{
+				$key_words[] = trim($val);
+			}
+
+			if($debug)
+			{
+				print "found value of $sname ... ";
+			}
+			// remove all ignore keywords from the retuned CNAM and compare before and after
+			$test_string = str_ireplace($key_words,'',$sname);
+			if($test_string == $sname)
+			{
+				$caller_id = $sname;
+				if($debug)
+				{
+					print "determined good.<br>\n";
+				}
+			}
+			else if($debug)
+			{
+				print " contains flagged key words, returning nothing ";
+			}
+		
         }
         else if($debug)
         {
                 print "not found<br>\n";
         }
-    }
+    
 }
