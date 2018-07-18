3914886a48197e96cec9b384359c99a658c39d96
James Finstrom <jfinstrom@sangoma.com>
2018-07-18 23:07:02 +0000

    Backup fixes

--- Backup.php
+++ Backup.php
@@ -3,6 +3,6 @@ namespace FreePBX\modules\Superfecta;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
   public function runBackup($id,$transaction){
-    $this->addConfigs($this->FreePBX->Superfecta->dumpConfig());
+    $this->addConfigs($this->FreePBX->Superfecta->dumpConfigs());
   }
 }
