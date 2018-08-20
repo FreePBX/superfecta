dc9f0138148b59b864fbdac4e198e9f8bd604cb0
James Finstrom <jfinstrom@sangoma.com>
2018-08-20 17:41:54 +0000

    Fix Syntax error

--- Restore.php
+++ Restore.php
@@ -8,7 +8,7 @@ class Restore Extends Base\RestoreBase{
 
   public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
     $tables = array_flip($tables+$unknownTables);
-    if(!isset(tables['superfectaconfig'])){
+    if(!isset($tables['superfectaconfig'])){
       return $this;
     }
     $bmo = $this->FreePBX->Superfecta;
