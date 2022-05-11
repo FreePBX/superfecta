b8a8b7b4e4330fa31789a0ca8128db33a006152a
Megha BM <meghabm@sangoma.com>
2022-05-11 16:35:36 +0530

    FREEI-4793 Fixed styling issues in GUI

--- views/header.php
+++ views/header.php
@@ -4,8 +4,8 @@
 		<?php foreach($schemes as $scheme) {?>
 		<li id="scheme_<?php echo $scheme['scheme']?>" class="scheme" data-name="<?php echo $scheme['name']?>">
 			<i class="fa fa-toggle-<?php echo $scheme['powered'] ? 'on' : 'off'?>" data-type="power"></i>
-			<i class="fa fa-arrow-down <?php echo ($scheme['showdown']) ? '' : 'hidden'?>" data-type="down"></i>
-			<i class="fa fa-arrow-up <?php echo ($scheme['showup']) ? '' : 'hidden'?>" data-type="up"></i>
+			<i class="fa fa-arrow-down <?php echo ($scheme['showdown']) ? '' : 'd-none'?>" data-type="down"></i>
+			<i class="fa fa-arrow-up <?php echo ($scheme['showup']) ? '' : 'd-none'?>" data-type="up"></i>
 			<i class="fa fa-files-o" data-type="duplicate"></i>
 			<i class="fa fa-trash-o" data-type="delete"></i>
 			<span onclick="window.location.href='config.php?display=superfecta&amp;action=edit&amp;scheme=<?php echo urlencode($scheme['name'])?>';"><?php echo $scheme['name']?></span>
